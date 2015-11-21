fea390b5482913bcdc8dfaf986915243d7c8bbf6
chihmin <s102062111@m102.nthu.edu.tw>
2015-11-21 21:39:46 +0800

    add mutex_lock/unlock macro to reduce code, and finish output dependence

--- hw2/hw2_SRCC.cpp
+++ hw2/hw2_SRCC.cpp
@@ -7,6 +7,8 @@
 #include <ctime>
 
 #define ATOI(params, i) params = atoi(argv[i])
+#define LOCK(mutex) pthread_mutex_lock(&mutex)
+#define UNLOCK(mutex) pthread_mutex_unlock(&mutex)
 
 using namespace std;
 
@@ -16,6 +18,7 @@ int curPeople;
 pthread_mutex_t carMutex;
 pthread_mutex_t curPeopleMutex;
 pthread_mutex_t passMutex[20];
+pthread_mutex_t printMutex;
 
 pthread_cond_t passCond[20];
 
@@ -28,44 +31,52 @@ vector <long> passList;
 void *passenger(void *ID) {
   bool isWalking = true;
   long passID = (long)(ID);
-  printf("Passenger ID : %d start\n", passID);
   
   while (!isCarStop) {
      
     // Walking arround
-    printf("Passenger [%d] : Walking arround\n", passID);
     int sleepTime = rand() % 100 + 1;
     isWalking = true;
     usleep(sleepTime);
-    pthread_mutex_lock(&curPeopleMutex);
+    
+    LOCK(curPeopleMutex);
+      LOCK(printMutex); 
+        printf("%d'th passenger returns for another ride\n", passID);
+      UNLOCK(printMutex);
+      
       if (!isCarStop && curPeople < C) {
         // If there's seat to jump into car
         curPeople++;
-        printf("Passenger [%d] : Go on the car, stop walking\n", passID);
+        //printf("Passenger [%d] : Go on the car, stop walking\n", passID);
         passList.push_back(passID);
         isWalking = false;      
       }
-    pthread_mutex_unlock(&curPeopleMutex);
+    UNLOCK(curPeopleMutex);
     
     if (!isWalking) { 
       pthread_mutex_lock(&passMutex[passID]);
-        printf("Passenger [%d] : Down from the car, Entering mutex\n", passID);
+        //printf("Passenger [%d] : Down from the car, Entering mutex\n", passID);
         pthread_cond_wait(&passCond[passID], &passMutex[passID]);
         isWalking = true;
-        printf("Passenger [%d] : Down from the car, stop walking\n", passID);
+        //printf("Passenger [%d] : Down from the car, stop walking\n", passID);
       pthread_mutex_unlock(&passMutex[passID]);
-      printf("Passenger [%d] : Mutex unlock from the car, stop walking\n", passID);
+      //printf("Passenger [%d] : Mutex unlock from the car, stop walking\n", passID);
+    } 
+    else {
+      LOCK(printMutex); 
+        printf("%d'th Passenger Walking arround\n", passID);
+      UNLOCK(printMutex);
     }
   }
    
-  printf("Passenger ID : %d Exit\n", passID);
+  // printf("Passenger ID : %d Exit\n", passID);
   pthread_exit(NULL);
 }
 
 void *car(void *runTimes) {
   int curTime = 0;
   while (N--) {
-    printf("Times %d\n", N);
+    //printf("Times %d\n", N);
     
     bool isFull = false;
     while (!isFull) {
@@ -75,34 +86,40 @@ void *car(void *runTimes) {
         isFull = true;
       pthread_mutex_unlock(&curPeopleMutex);
     }
-     
-    printf("car departures at %d millisec. PeupleNum = %d ", curTime, curPeople);
-    for (int i = 0; i < passList.size(); ++i) {
-      printf("%dth, ", passList[i]);
-    } printf("are in the car\n");
-    
+
+    LOCK(printMutex); 
+      printf("car departures at %d millisec. ", curTime, curPeople);
+      for (int i = 0; i < passList.size(); ++i) 
+        printf("%dth, ", passList[i]); 
+      printf("are in the car\n");
+    UNLOCK(printMutex);
+
     usleep(T); // now car Running
     curTime += T;
-    printf("car arrives at %d millisec. ", curTime);
+    
     for (int i = 0; i < passList.size(); ++i) {
-      long passID = passList[i];
-      printf("%dth, ", passID);
       // Broadcast passenger down from car
-      pthread_mutex_lock(&passMutex[passID]);
+      long passID = passList[i];
+      LOCK(passMutex[passID]);
         pthread_cond_signal(&passCond[passID]);
-      pthread_mutex_unlock(&passMutex[passID]);
-      usleep(10);
-    } printf("should go off\n");
+      UNLOCK(passMutex[passID]);
+    } 
 
+    LOCK(printMutex); 
+      printf("car arrives at %d millisec. ", curTime);
+      for (int i = 0; i < passList.size(); ++i)
+        printf("%dth, ", passList[i]); 
+      printf("should go off\n");
+    UNLOCK(printMutex);
+    
     if (N != 0) { 
-      pthread_mutex_lock(&curPeopleMutex);
-      curPeople = 0; // release all people
-      pthread_mutex_unlock(&curPeopleMutex);
-      passList.clear();
+      LOCK(curPeopleMutex);
+        curPeople = 0; // release all people
+        passList.clear();
+      UNLOCK(curPeopleMutex);
     }
   }
   isCarStop = true;
-  printf("Passenger ID : CAR Exit\n");
   pthread_exit(NULL);
 }
 
@@ -119,6 +136,7 @@ int main(int argc, char *argv[]) {
   // srand(time(0));
   pthread_mutex_init(&carMutex, NULL);
   pthread_mutex_init(&curPeopleMutex, NULL);
+  pthread_mutex_init(&printMutex, NULL);
   for (int i = 0 ; i < n; ++i) {
     pthread_cond_init(&passCond[i], NULL);
     pthread_mutex_init(&passMutex[i], NULL);
